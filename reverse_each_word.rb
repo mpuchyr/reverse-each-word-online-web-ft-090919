b2dee650f5e8f759af5444a61e81d095db9861b7
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-09 23:38:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
-def reverse_each_word(words)
+def reverse_each_word(string)
+  words = string.split
+  reversed_string = ""
+  words.each do |word|
+    reversed_string << word.reverse
+  end
   
+  reversed_string.join(" ")
 end
